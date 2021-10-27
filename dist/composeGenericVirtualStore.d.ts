791de59efadea81fd817a0a715d2b008352d1a44
Paul Wand <66976845+SilentEsper@users.noreply.github.com>
2021-10-27 12:13:53 -0600

    re-compile

--- dist/composeGenericVirtualStore.d.ts
+++ dist/composeGenericVirtualStore.d.ts
@@ -1,9 +1,9 @@
-import { StateCreator, UseStore } from "zustand";
+import { StateCreator, UseBoundStore } from "zustand";
 import { composeVirtualStoreOptions } from "./composeStoreOptions";
 import { VirtualStore } from "./virtual-store";
 /**
  * @param synchronize function to synchronize into real object
  * @param fetch get the value of the real object
  */
-declare const composeGenericVirtualStore: <StoreType, DataType>(create: (storeCreator: StateCreator<VirtualStore<DataType>, import("zustand").SetState<VirtualStore<DataType>>, import("zustand").GetState<VirtualStore<DataType>>, import("zustand").StoreApi<VirtualStore<DataType>>>) => UseStore<VirtualStore<DataType>, import("zustand").StoreApi<VirtualStore<DataType>>>, options: composeVirtualStoreOptions<DataType>) => UseStore<VirtualStore<DataType>, import("zustand").StoreApi<VirtualStore<DataType>>>;
+declare const composeGenericVirtualStore: <StoreType, DataType>(create: (storeCreator: StateCreator<VirtualStore<DataType>, import("zustand").SetState<VirtualStore<DataType>>, import("zustand").GetState<VirtualStore<DataType>>, import("zustand").StoreApi<VirtualStore<DataType>>>) => UseBoundStore<VirtualStore<DataType>, import("zustand").StoreApi<VirtualStore<DataType>>>, options: composeVirtualStoreOptions<DataType>) => UseBoundStore<VirtualStore<DataType>, import("zustand").StoreApi<VirtualStore<DataType>>>;
 export { composeGenericVirtualStore };
